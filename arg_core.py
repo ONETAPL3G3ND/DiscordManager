a65ceaf365282056cf1047bf2c73755dd0409809
Sergii Rudniev <100538048+ONETAPL3G3ND@users.noreply.github.com>
2024-05-07 19:43:41 +0200

    Add files via upload

--- arg_core.py
+++ arg_core.py
@@ -4,22 +4,20 @@ def get_arg(ctx):
     command = ctx.message.content
     command = command.split(' ', 1)[-1]
 
-    args = command.split('-')  # Разделение по "-"
+    args = command.split('-')
     parsed_args = []
 
     for arg in args:
-        # Удаляем начальные и конечные пробелы и символы кавычек
         arg = arg.strip().strip('"')
 
-        # Для параметров вида amount(99) или bool(True)
         if '(' in arg:
             key, value = arg.split('(')
             value = value.rstrip(')')
-            if value.isdigit():  # Если значение - число, преобразуем в int
+            if value.isdigit():
                 value = int(value)
-            elif value.lower() == 'true':  # Если значение - 'true', возвращаем True
+            elif value.lower() == 'true':
                 value = True
-            elif value.lower() == 'false':  # Если значение - 'false', возвращаем False
+            elif value.lower() == 'false':
                 value = False
             parsed_args.append((key.strip(), value))
         else:
